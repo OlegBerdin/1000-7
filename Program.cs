f1bbbac22cb8e856c64269ae13c102dd42e81f83
OlegBerdin <85506312+OlegBerdin@users.noreply.github.com>
2021-09-11 14:31:54 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,33 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace ConsoleApplication1
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
-        {
-            Console.Write("Напипшите слово: ");
-            var str = Console.ReadLine();
-            var len = str.Length;
-            var t = true;
+{           bool r;
+            string []s = File.ReadAllLines(@"X:\s.txt");
+            do
+            {
+                foreach (var str in s)
+                {   
 
-            for(int i=0; i< len/2; i++)
-            {
-                if (str[i] != str[len - (i + 1)])
-                    t = false;
-                break;
+                    var t = true;
+
+                    for (int i = 0; i < str.Length / 2; i++)
+                    {
+                        if (str[i] != str[str.Length - (i + 1)])
+                            t = false;
+                        break;
+                    }
+                    if (t)
+                        Console.WriteLine("{0} является палиндромом", str);
+                    else
+                        Console.WriteLine("{0} не является палиндромом", str);
+                }
+                Console.WriteLine("Продолжить? Да - 1, нет - 0 ");
+                int pr = Int32.Parse(Console.ReadLine());
+                if (pr == 1) r = true;
+                else r = false;
+                Console.Clear();
             }
-            if (t )
-            {
-                Console.WriteLine("{0} является палиндромом", str);
-            }
-            else
-            {
-                Console.WriteLine("{0} не является палиндромом", str);
-            }
-            Console.ReadLine();
+            while (r == true);
+                Console.ReadLine();
+            
         }
     }
 }
